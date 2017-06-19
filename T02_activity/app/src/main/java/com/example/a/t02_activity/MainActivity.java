7f7d97b6622f04a1488bd6917f5aca5af40680b7
Your Name <yourfunny@naver.com>
2017-06-19 18:29:05 +0900

    0619

---
+++ T02_activity/app/src/main/java/com/example/a/t02_activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a.t02_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onBtnClick(View view){
+        Intent intent = new Intent(MainActivity.this, MyActivity.class);
+        intent.putExtra("myValue", "Hello World");
+        intent.putExtra("secondValue", 123);
+        startActivity(intent);
+    }
+}
