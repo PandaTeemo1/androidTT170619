ff77ef6cb7524ea7f7c4799f59885d6449cdd319
Your Name <yourfunny@naver.com>
2017-06-20 19:24:14 +0900

    0619

--- T02_activity/app/src/main/java/com/example/a/t02_activity/MainActivity.java
+++ T02_activity/app/src/main/java/com/example/a/t02_activity/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int myRequest = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("myValue", "Hello World");
         intent.putExtra("secondValue", 123);
         startActivity(intent);
+
+        startActivityForResult(intent, myRequest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == myRequest){
+            if(resultCode == RESULT_OK){
+                String res = data.getStringExtra("myResult");
+                Toast.makeText(this, "res "+res, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
